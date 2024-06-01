129206d83aec49ffd86f76765856dc32e1b084e0
da-tra <dtraska@gmail.com>
2024-06-01 21:03:14 +0200

    experiment with PrettyTable methods

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from prettytable import PrettyTable
 table = PrettyTable() 
 table.add_column("Pokemon", ["Pikachu", "Squirtle", "Charmander"]), table.add_column("Type", ["Electric", "Water", "Fire"])
 
-
-
+table.align = "l"
+table.vrules = True
+table.vertical_char = "$"
+table.horizontal_char = "$"
+junction_char = "$"
+print(table.align)
+print(table.vrules)
 print(table)
